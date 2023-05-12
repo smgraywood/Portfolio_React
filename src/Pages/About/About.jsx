d09c6fd212135970ef077e5bde3baae9a4c71396
Sarah Graywood <smgraywood@gmail.com>
2023-05-11 20:30:36 -0700

    add quote api call

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,26 @@
-import HeaderImg from "../../Components/HeaderImg/HeaderImg"
+import HeaderImg from "../../Components/HeaderImg/HeaderImg";
 
 const About = () => {
+	return (
+		<div className="About">
+			<h1>About</h1>
+            
+			<HeaderImg />
+			<div className="aboutme">
+				<p>I’ve been coding for almost 3 years.</p>
+				<p>I care for 2 cats and over 100 succulents and cacti.</p>
+				<p>I love design, art, color, and fashion.</p>
+				<p>I joyfully move my body via Latin dance.</p>
+				<p>I live in San Jose with my wonderful husband.</p>
+				<p>I am proud of having grown up in Watsonville, CA.</p>
+				<p>
+					I am passionate about POC, women, LGBTQ+ and other
+					historically underrepresented groups in tech taking up space
+					here.
+				</p>
+			</div>
+		</div>
+	);
+};
 
-    return(
-        <div>
-        <h1>About</h1>
-        <HeaderImg/>
-        </div>
-    )
-}
-
-export default About
+export default About;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,63 @@
+import { useSearchParams } from "react-router-dom";
 import skyline from "../../Images/skyline.jpeg";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = () => {
+function Home() {
+	const [quote, setQuote] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	// const category = "inspirational"
+	const options = {
+		method: "GET",
+		url: "https://api.api-ninjas.com/v1/quotes?category=",
+		params: {
+			category: "inspirational",
+			limit: "1",
+		},
+		headers: {
+			"X-Api-Key": "IVtQmp37gd3NE3SPDKRrLQ==79hqzLO1QtGnyTs9",
+		},
+	};
+	const apiCall = async () => {
+		try {
+			const response = await axios.request(options);
+			setQuote(response.data);
+            setLoading(!loading)
+			console.log(quote);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	useEffect(() => {
+		apiCall();
+	}, []);
 
 	return (
 		<>
-			<h1>Home</h1>
-			<img src={skyline} className="skyline" alt="drawn outline of San Jose CA skyline"></img>
+			{loading ? (
+				<>
+					<h1>Home</h1>
+					<img
+						src={skyline}
+						className="skyline"
+						alt="drawn outline of San Jose CA skyline"
+					></img>
+					<p>{quote[0].quote}</p>
+					<p>{quote[0].author}</p>{" "}
+				</>
+			) : (
+				<>
+					<h1>Home</h1>
+					<img
+						src={skyline}
+						className="skyline"
+						alt="drawn outline of San Jose CA skyline"
+					></img>
+				</>
+			)}
 		</>
 	);
-};
+}
 
 export default Home;
